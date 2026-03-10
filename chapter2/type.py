8cddd6d7c161ca89d4b61edad83554f65c934c5e
subhamsi <sanjusinghkk3498@gmail.com>
2026-03-10 19:12:49 +0530

    new chapter

---
+++ chapter2/type.py
@@ -0,0 +1,14 @@
+l  = 23
+print(type(l))  # <class 'int'>
+
+# type converstion
+f = float(l)
+print(f)  # 23.0
+print(type(f))  # <class 'float'>
+s = str(l)
+print(s)  # '23'
+print(type(s))  # <class 'str'>
+b = bool(l)
+print(b)  # True
+print(type(b))  # <class 'bool'>
+# type() can also be used to create new types
---
+++ chapter2/typeconversitio.py
@@ -0,0 +1,6 @@
+a = "3213"
+b = float(a) # tthis code make this string into  float number 
+print(b)
+aa =  23213 # this is a number and we convert it into a string
+bb = int(aa)
+print(bb)
