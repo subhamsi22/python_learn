662313a6f9bbccdb5ae829e253b7c63e0f69e9e8
subhamsi <sanjusinghkk3498@gmail.com>
2026-03-10 09:59:43 +0530

    my first day of learning python

---
+++ chapter_1/pr_2.py
@@ -0,0 +1,4 @@
+import pyttsx3
+a  = pyttsx3.init()
+a.say("Hello, I am a text to speech conversion program. I can read out any text you want me to. Just type in the text and I will read it out for you.")
+a.runAndWait()
