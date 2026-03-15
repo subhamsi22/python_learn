2e5ac9573e297552fde980ee4f2b85264e84e39e
subhamsi <sanjusinghkk3498@gmail.com>
2026-03-15 09:07:10 +0530

    scape sequence

---
+++ scape_sec.py
@@ -0,0 +1,23 @@
+
+# \\ back slash
+# \' single quate
+# \" duable quate
+# \n new line
+# \t horizontal tab
+# \r carriage return
+# \b backspace
+# \f form feed
+# \a bell alert sound
+# \v vertical tab
+# \ooo character with octal value
+# \xhh character with her value
+# \N{name} unicode chrter by name
+# \uXXXX unicode 16but hex 
+# \UXXXXXXXX unicode 32 bit hex
+
+
+
+
+
+a  = "\a subham singh ia a smart boy \n not a \"bad boy\" \t he is a very smart boy"
+print(a)
