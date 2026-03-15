b27a33bc80843febab25497afbfaebf1462cf87b
subhamsi <sanjusinghkk3498@gmail.com>
2026-03-15 08:53:30 +0530

    the new updte

--- chapter2/pr_5.py
+++ chapter2/pr_5.py
@@ -1,4 +1,6 @@
 # 6. Write a python program to calculate the square of a number entered by the user.
 a  = int(input("enter the number"))
 c  = a*a
+d =  a**a
 print(c)
+print(d)
---
+++ chapter3_string/string_types.py
@@ -0,0 +1,25 @@
+# we can write string in 3 ways 
+a  = 'subham singh' # using single quests
+b = "subham singh"  # using duble quests 
+c  = ''' subham singh''' # using tripel quets
+#strig fucntion 
+#1
+str = "subham" # it retun 6 the total length
+print(len(str))
+str2 = "subham"
+if str2.endswith("am"):
+    print("yes")
+
+str3 ="subham"
+count = str3.count("u")
+print(count)
+
+
+str4  =  "subham singh"
+index = str4.find("bh") #use to fint the index number using the string
+print(index)
+
+
+str5 = "subham singh"
+rep = str.replace("subham", "sanju")
+print(rep )
